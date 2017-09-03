e1962dfacacff8ec9b4cddfe7c3441ecd105924d
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2017-09-03 11:48:14 +0200

    Implement main logic and parsing

--- index.js
+++ index.js
@@ -14,6 +14,106 @@ function LineChatParser(users) {
         return new LineChatParser(users);
     }
     this.users = users;
+    this.currentDate = new Date();
+    this.currentMessage = null;
 }
 
 LineChatParser.prototype = Object.create(events.EventEmitter.prototype);
+
+/**
+ * Process the given chat line. This emits a 'message' event when the end of a
+ * previously started message has been detected. The line can be a normal
+ * message, a message continuation, or a date header.
+ *
+ * Make sure to call flush() when all lines have been processed.
+ *
+ * @param {string} line - The chat export line.
+ */
+LineChatParser.prototype.process = function (line) {
+
+    var match;
+
+    // date header
+    match = line.match(/^(\d{4})\.(\d{2})\.(\d{2}) [A-Z][a-z]+/);
+    if (match) {
+        var year = parseInt(match[1], 10),
+            month = parseInt(match[2], 10) - 1,
+            day = parseInt(match[3], 10);
+        this.processDateHeader(year, month, day);
+        return;
+    }
+
+    // message
+    match = line.match(/^(\d{2}):(\d{2}) (\S.+)/);
+    if (match) {
+        var hours = parseInt(match[1], 10),
+            minutes = parseInt(match[2], 10);
+        var author = "";
+        this.users.forEach(function (user) {
+            var prefix = user + " ";
+            if (user.length > author.length && match[3].indexOf(prefix) === 0) {
+                author = user;
+            }
+        });
+        var text = match[3].substring(author.length + 1);
+        this.processMessageStart(hours, minutes, author, text);
+        return;
+    }
+
+    this.processMessageContinuation(line);
+
+};
+
+/**
+ * Processes a date header, given its tokens.
+ *
+ * @param {number} year - The year.
+ * @param {number} month - The 0-based month (0 for January).
+ * @param {number} day - The 1-based day.
+ */
+LineChatParser.prototype.processDateHeader = function (year, month, day) {
+    this.flush();
+    this.currentDate = new Date(year, month, day);
+};
+
+/**
+ * Processes a message, given its tokens.
+ *
+ * @param {number} hours - The hour at which the message was sent (0 to 24).
+ * @param {number} minutes - The minute at which the message was sent (0 to 59).
+ * @param {string} author - The message author.
+ * @param {string} text - The message text.
+ */
+LineChatParser.prototype.processMessageStart = function (hours, minutes, author, text) {
+    this.flush();
+    var date = new Date(this.currentDate.getTime());
+    date.setHours(hours, minutes);
+    this.currentMessage = {
+        date: date,
+        author: author,
+        text: text,
+    };
+};
+
+/**
+ * Processes further text belonging to the previously started message.
+ *
+ * @param {string} text - The message text to append.
+ */
+LineChatParser.prototype.processMessageContinuation = function (text) {
+    if (this.currentMessage) {
+        this.currentMessage.text += "\n" + text;
+    }
+};
+
+/**
+ * Ends and emits the current message, if one exists.
+ * Call this after all lines have been processed. Otherwise, the message will
+ * only be emitted when further lines mark the beginning of a new message.
+ */
+LineChatParser.prototype.flush = function () {
+    if (this.currentMessage) {
+        this.emit("message", this.currentMessage);
+        this.currentMessage = null;
+    }
+};
--- test/index.js
+++ test/index.js
@@ -20,4 +20,100 @@ describe("LineChatParser", function () {
         });
     });
 
+    describe("#process()", function () {
+
+        it("should process date headers", function () {
+            var obj = new LineChatParser(["foo"]);
+            obj.process("2017.09.02 Saturday");
+            // month is 0-based
+            expect(obj.currentDate).to.deep.equal(new Date(2017, 8, 2));
+        });
+
+        it("should process message starts", function () {
+            var obj = new LineChatParser(["foo"]);
+            obj.process("12:00 foo hello world");
+            expect(obj.currentMessage).to.be.an("object").that.includes({
+                author: "foo",
+                text: "hello world",
+            });
+        });
+
+        it("should extend the base date in message starts", function () {
+            var obj = new LineChatParser(["foo"]);
+            obj.currentDate = new Date(2017, 8, 2);
+            obj.process("12:00 foo hello world");
+            expect(obj.currentMessage.date).to.deep.equal(new Date(2017, 8, 2, 12, 0));
+        });
+
+        it("should pick the longest matching user", function () {
+            var obj = new LineChatParser(["foo", "bar", "foo bar"]);
+            obj.process("12:00 foo hello world");
+            expect(obj.currentMessage.author).to.equal("foo");
+            obj.process("12:00 bar hello world");
+            expect(obj.currentMessage.author).to.equal("bar");
+            obj.process("12:00 foo bar hello world");
+            expect(obj.currentMessage.author).to.equal("foo bar");
+        });
+
+        it("should allow special characters in user names", function () {
+            var obj = new LineChatParser(["foo", "test_:)3 name"]);
+            obj.process("12:00 test_:)3 name hello world");
+            expect(obj.currentMessage.author).to.equal("test_:)3 name");
+        });
+
+        it("should concat message continuations", function () {
+            var obj = new LineChatParser(["foo"]);
+            obj.process("12:00 foo first line");
+            obj.process("second line");
+            obj.process("third line");
+            expect(obj.currentMessage.text).to.equal("first line\nsecond line\nthird line");
+        });
+
+        it("should emit the message when a new one starts", function (done) {
+            var obj = new LineChatParser(["foo"]);
+            obj.on("message", function (msg) {
+                //console.log(msg);
+                expect(msg).to.be.an("object").that.includes({
+                    author: "foo",
+                    text: "hello other",
+                });
+                done();
+            });
+            obj.process("12:00 foo hello other");
+            obj.process("12:00 foo other");
+        });
+
+        it("should emit the message when reaching a date header", function (done) {
+            var obj = new LineChatParser(["foo"]);
+            obj.on("message", function (msg) {
+                expect(msg).to.be.an("object").that.deep.includes({
+                    author: "foo",
+                    text: "hello date",
+                });
+                done();
+            });
+            obj.process("12:00 foo hello date");
+            obj.process("2017.09.02 Saturday");
+        });
+
+    });
+
+    describe("#flush()", function () {
+
+        it("should emit the message", function (done) {
+            var obj = new LineChatParser(["foo"]);
+            var message = obj.currentMessage = {
+                author: "foo",
+                text: "hello world",
+                date: new Date(),
+            };
+            obj.on("message", function (msg) {
+                expect(msg).to.equal(message);
+                done();
+            });
+            obj.flush();
+        });
+
+    });
+
 });
